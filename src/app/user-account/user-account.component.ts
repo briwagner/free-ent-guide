95c820e215a7d215d3a768b1e4d605d4093818e6
Brian Wagner <briwagner@outlook.com>
2025-05-05 14:34:03 -0500

    styles; hide zip widget

--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -13,171 +13,179 @@ import { timeStamp } from 'console';
 })
 export class UserAccountComponent implements OnInit {
 
-  flash: Flash;
-  formError: boolean = false;
-  newZip: string;
-  username: string;
-  userToken: any;
-  userZips: Array<string>;
-  userShows: Array<number>;
-  userShowDetails: Array<Show>;
+	flash: Flash;
+	formError: boolean = false;
+	newZip: string;
+	showZips: boolean = false;
+	username: string;
+	userToken: any;
+	userZips: Array<string>;
+	userShows: Array<number>;
+	userShowDetails: Array<Show>;
 
-  constructor(
-    private userservice: UserService,
-    private tvservice: TvShowSearchService,
-    private router: Router
-  ) { }
+	constructor(
+		private userservice: UserService,
+		private tvservice: TvShowSearchService,
+		private router: Router
+	) { }
 
-  ngOnInit(): void {
-    if (localStorage.getItem('entToken') === null) {
-      this.userToken = null;
-    } else {
-      this.userToken = localStorage.getItem('entToken');
-    }
+	ngOnInit(): void {
+		if (localStorage.getItem('entToken') === null) {
+		this.userToken = null;
+		} else {
+		this.userToken = localStorage.getItem('entToken');
+		}
 
-    this.flash = new Flash;
-    this.userZips = [];
-    this.userShows = [];
-    this.userShowDetails = [];
-    this.fetchUserData();
-  }
+		this.flash = new Flash;
+		this.userZips = [];
+		this.userShows = [];
+		this.userShowDetails = [];
+		this.fetchUserData();
+	}
 
-  /**
-   * Get stored user data.
-   */
-  fetchUserData() {
-    this.username = this.userservice.checkUser(this.userToken);
-    if (this.username) {
-      this.userservice.fetchUserData(this.userToken)
-        .subscribe(
-          p => {
-            this.userZips = p['zipcodes'];
-            this.userShows = p['shows'];
-            this.fetchShowData(p['shows']);
-          },
-          e => {
-            if (e.status == '401') {
-              this.flash.message = "Not authorized. Please <a href='/user/login/'>log in to continue</a>"
-              this.flash.status = 'warning'
-              return
-            }
-            if (e.status == 404) {
-              this.flash.message = 'No saved user data.'
-              this.flash.status = 'info';
-              return
-            }
-            this.flash.message = "Unable to load user data."
-            this.flash.status = 'warning';
-          }
-        )
-    } else {
-      console.log("Cannot get username")
-    }
-  }
+	/**
+	 * Get stored user data.
+	 */
+	fetchUserData() {
+		this.username = this.userservice.checkUser(this.userToken);
+		if (this.username) {
+		this.userservice.fetchUserData(this.userToken)
+			.subscribe(
+			p => {
+				this.userZips = p['zipcodes'];
+				this.userShows = p['shows'];
+				this.fetchShowData(p['shows']);
+			},
+			e => {
+				if (e.status == '401') {
+				this.flash.message = "Not authorized. Please <a href='/user/login/'>log in to continue</a>"
+				this.flash.status = 'warning'
+				return
+				}
+				if (e.status == 404) {
+				this.flash.message = 'No saved user data.'
+				this.flash.status = 'info';
+				return
+				}
+				this.flash.message = "Unable to load user data."
+				this.flash.status = 'warning';
+			}
+			)
+		} else {
+		console.log("Cannot get username")
+		}
+	}
 
-  /**
-   * Fetch show data
-   */
-  fetchShowData(ids: Array<string>) {
-    if (ids.length == 0) {
-      console.log("no user shows")
-      return
-    }
-    for (let i = 0; i < ids.length; i++) {
-      this.tvservice.getShow(ids[i])
-          .subscribe(
-            p => {
-              this.userShowDetails.push(p)
-            },
-            e => console.log(e)
-          )
-    }
-  }
+	/**
+	 * Fetch show data
+	 */
+	fetchShowData(ids: Array<string>) {
+		if (ids.length == 0) {
+		console.log("no user shows")
+		return
+		}
+		for (let i = 0; i < ids.length; i++) {
+		this.tvservice.getShow(ids[i])
+			.subscribe(
+				p => {
+				this.userShowDetails.push(p)
+				},
+				e => console.log(e)
+			)
+		}
+	}
 
-  /**
-   * Store new zip in user storage.
-   *
-   * @param {string} newZip
-   */
-  storeZip(newZip: string) {
-    this.clearFlash();
+	/**
+	 * Store new zip in user storage.
+	 *
+	 * @param {string} newZip
+	 */
+	storeZip(newZip: string) {
+		this.clearFlash();
 
-    if (!this.validZip(newZip)) {
-      this.flash.message = 'Please enter a valid zip';
-      this.flash.status = 'warning';
-      return;
-    }
+		if (!this.validZip(newZip)) {
+		this.flash.message = 'Please enter a valid zip';
+		this.flash.status = 'warning';
+		return;
+		}
 
-    this.userservice.saveUserZip(this.userToken, newZip)
-      .subscribe(
-        p => {
-          this.userZips = p;
-        },
-        e => {
-          this.flash.message = 'Error saving zip';
-          this.flash.status = 'warning';
-        },
-        () => {
-          this.newZip = '';
-        }
-      )
-  }
+		this.userservice.saveUserZip(this.userToken, newZip)
+		.subscribe(
+			p => {
+			this.userZips = p;
+			},
+			e => {
+			this.flash.message = 'Error saving zip';
+			this.flash.status = 'warning';
+			},
+			() => {
+			this.newZip = '';
+			}
+		)
+	}
 
-  /**
-   * Validate zip code entered in form field.
-   * @param {string} zip
-   * @return {boolean}
-   */
-     validZip(zip: string) {
-      if (zip != undefined) {
-        if (zip.toString().length == 5) {
-          var isNumber = /\d/;
-          return isNumber.test(zip);
-        }
-      }
-      return false;
-    }
+	/**
+	 * Validate zip code entered in form field.
+	 * @param {string} zip
+	 * @return {boolean}
+	 */
+		validZip(zip: string) {
+		if (zip != undefined) {
+			if (zip.toString().length == 5) {
+			var isNumber = /\d/;
+			return isNumber.test(zip);
+			}
+		}
+		return false;
+		}
 
-  /**
-   * Clear flash message.
-   */
-  clearFlash() {
-    this.flash = new Flash
-  }
+	/**
+	 * Button handler.
+	 */
+	showZipFields() {
+		this.showZips = !this.showZips
+	}
 
-  /**
-   * Delete zip from user storage.
-   *
-   * @param {string} zip
-   */
-  deleteZip(zip: string) {
-    this.clearFlash();
+	/**
+	 * Clear flash message.
+	 */
+	clearFlash() {
+		this.flash = new Flash
+}
 
-    this.userservice.deleteZip(this.userToken, zip)
-      .subscribe(
-        p => {
-          this.flash.message = `Zip ${zip} removed`;
-          this.flash.status = 'info';
-          if (p) {
-            this.userZips = p;
-          }
-        },
-        e => {
-          this.flash.message = `Error: ${e}`;
-          this.flash.status = 'warning';
-        }
-      )
-  }
+	/**
+	 * Delete zip from user storage.
+	 *
+	 * @param {string} zip
+	 */
+	deleteZip(zip: string) {
+		this.clearFlash();
 
-  /**
-   * Delete token.
-   */
-  logout() {
-    localStorage.removeItem('entToken')
-    this.userservice.logoutUser(this.userToken).subscribe(
-      p => this.router.navigate(['/']),
-      e => console.log(e)
-    )
-  }
+		this.userservice.deleteZip(this.userToken, zip)
+		.subscribe(
+			p => {
+			this.flash.message = `Zip ${zip} removed`;
+			this.flash.status = 'info';
+			if (p) {
+				this.userZips = p;
+			}
+			},
+			e => {
+			this.flash.message = `Error: ${e}`;
+			this.flash.status = 'warning';
+			}
+		)
+	}
+
+	/**
+	 * Delete token.
+	 */
+	logout() {
+		localStorage.removeItem('entToken')
+		this.userservice.logoutUser(this.userToken).subscribe(
+		p => this.router.navigate(['/']),
+		e => console.log(e)
+		)
+	}
 
 }
